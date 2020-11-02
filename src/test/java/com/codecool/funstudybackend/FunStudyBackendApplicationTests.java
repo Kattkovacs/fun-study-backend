9ead47780624ac3a8dc3fd0b9f68b2d8b05a9134
Kattkovacs <kattkovacs@gmail.com>
2020-11-02 14:14:51 +0100

    init

---
+++ src/test/java/com/codecool/funstudybackend/FunStudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.funstudybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunStudyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
