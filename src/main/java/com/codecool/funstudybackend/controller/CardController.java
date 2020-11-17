47d8bd30f240a2110a9899dccb34274bf360c71b
Kattkovacs <kattkovacs@gmail.com>
2020-11-17 10:58:19 +0100

    add UserRepository to CardController

--- src/main/java/com/codecool/funstudybackend/controller/CardController.java
+++ src/main/java/com/codecool/funstudybackend/controller/CardController.java
@@ -1,17 +1,22 @@
 package com.codecool.funstudybackend.controller;
 
+import com.codecool.funstudybackend.repository.UserRepository;
 import com.codecool.funstudybackend.service.RemoteURLReader;
 import com.codecool.funstudybackend.entity.User;
 import com.codecool.funstudybackend.service.APIService;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 import com.github.dhiraj072.randomwordgenerator.RandomWordGenerator;
 
+@EnableJpaRepositories(basePackages = "repository")
+@Service
 @CrossOrigin
 @RestController
 public class CardController {
@@ -22,7 +27,7 @@ public class CardController {
     APIService apiService;
 
     @Autowired
-    User user;
+    private UserRepository userRepository;
 
     @GetMapping("/card")
     public ObjectNode createCardContent() throws IOException {
@@ -32,12 +37,13 @@ public class CardController {
 
     @PostMapping(value = "/registration", consumes = "application/json", produces = "application/json")
     public User registration(@RequestBody User user) {
-        this.user = user;
+        userRepository.save(user);
         return user;
     }
 
     @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Boolean> login(@RequestBody User user) {
-        return ResponseEntity.ok(this.user.getEmail().equals(user.getEmail()) && this.user.getPassword().equals(user.getPassword()));
+        User userByEmailAndPassword = userRepository.findUserByEmailAndPassword(user.getEmail(), user.getPassword());
+        return ResponseEntity.ok(userByEmailAndPassword != null);
     }
 }
--- src/main/java/com/codecool/funstudybackend/repository/UserRepository.java
+++ src/main/java/com/codecool/funstudybackend/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.codecool.funstudybackend.repository;
 
 import com.codecool.funstudybackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findUserByEmailAndPassword(String email, String password);
 }
