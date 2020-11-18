cae490550e82db8ff2c0ec2b5aa4f195c3566acc
Eszkis <laszlo.szkis@gmail.com>
2020-11-18 14:02:34 +0100

    refactor dbinit

--- src/main/java/com/codecool/funstudybackend/util/DbInit.java
+++ src/main/java/com/codecool/funstudybackend/util/DbInit.java
@@ -6,7 +6,9 @@ import com.codecool.funstudybackend.service.APIService;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.dhiraj072.randomwordgenerator.RandomWordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Component
 public class DbInit {
@@ -16,28 +18,25 @@ public class DbInit {
     @Autowired
     private APIService apiService;
 
-    private int cardCount = 10;
     private String[] pictureWords = new String[]{"lamp", "cat", "dog", "horse", "fish", "catfish", "hammer", "flag", "beer", "hamster"
-    ,"rabbit","mouse","rat","owl","crab","shark","jellyfish","turtle","car","bus"
-            ,"gun","shield","helmet","blue","red"
+            , "rabbit", "mouse", "rat", "owl", "crab", "shark", "jellyfish", "turtle", "car", "bus"
+            , "gun", "shield", "helmet", "blue", "red"
     };
 
-    public void initDb() {
+    public void initDb(int cardCount) {
         for (int i = 0; i < cardCount; i++) {
             ObjectNode cardData1 = apiService.findCardContentFromResult(apiService.askForCardJson(RandomWordGenerator.getRandomWord()));
             Card tempCard1 = Card.builder()
                     .word(cardData1.get("word").toString())
                     .definition(cardData1.get("definition").toString())
-                    .image_url(null)
+                    .imageUrl(null)
                     .build();
             cardRepository.save(tempCard1);
-        }
-        for (int i = 0; i < cardCount; i++) {
             ObjectNode cardData2 = apiService.findCardContentFromResult(apiService.askForCardJsonWithImg(pictureWords[i]));
             Card tempCard2 = Card.builder()
                     .word(cardData2.get("word").toString())
                     .definition(cardData2.get("definition").toString())
-                    .image_url(cardData2.get("image_url").toString())
+                    .imageUrl(cardData2.get("image_url").toString())
                     .build();
             cardRepository.save(tempCard2);
         }
