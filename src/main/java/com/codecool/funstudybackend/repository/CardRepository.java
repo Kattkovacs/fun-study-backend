5f1b5ae8880a6b2215b1f8b9d36e0866e44d1dff
Eszkis <laszlo.szkis@gmail.com>
2020-11-18 14:05:35 +0100

    add function to get card with or without image

--- src/main/java/com/codecool/funstudybackend/repository/CardRepository.java
+++ src/main/java/com/codecool/funstudybackend/repository/CardRepository.java
@@ -2,6 +2,15 @@ package com.codecool.funstudybackend.repository;
 
 import com.codecool.funstudybackend.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    @Query("SELECT c FROM Card c WHERE c.imageUrl NOT LIKE 'null'")
+    List<Card> getAllCardWithPicture();
+
+    @Query("SELECT c FROM Card c WHERE c.imageUrl LIKE 'null'")
+    List<Card> getAllCardWithOutPicture();
 }
